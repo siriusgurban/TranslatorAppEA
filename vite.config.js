df31131462b669acfbb8051f77cfa88e0fdde2dd
2883857@student.edu.az <2883857@student.edu.az>
2024-01-23 02:13:24 +0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/TranslatorAppEA/",
   plugins: [react()],
-  base: "/TranslatorAppEA/"
 })
